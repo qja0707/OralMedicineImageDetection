120fc60f8c36785e7f51718b6f78a7b654293e02
Gyubeom Park <qja0707@gmail.com>
2026-05-06 17:31:46 +0900

    fix: dir error

--- ml/scripts/2step_train/04_5_re_split_coco.py
+++ ml/scripts/2step_train/04_5_re_split_coco.py
@@ -1,11 +1,11 @@
 from pathlib import Path
-
-from ml.src.data.split_coco import split_coco_dataset
 import sys
 
 project_root = Path(__file__).resolve().parents[3]
 sys.path.insert(0, str(project_root))
 data_root = project_root / "ml" / "data"
+
+from ml.src.data.split_coco import split_coco_dataset
 
 def main():
 
