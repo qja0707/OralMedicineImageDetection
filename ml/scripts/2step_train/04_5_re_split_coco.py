128bd24951cf0a93a06795b60978bf2b6621a62b
Gyubeom Park <qja0707@gmail.com>
2026-05-06 17:25:30 +0900

    fix: wrong dir adress

--- ml/scripts/2step_train/04_5_re_split_coco.py
+++ ml/scripts/2step_train/04_5_re_split_coco.py
@@ -3,10 +3,11 @@ from pathlib import Path
 from ml.src.data.split_coco import split_coco_dataset
 import sys
 
+project_root = Path(__file__).resolve().parents[3]
+sys.path.insert(0, str(project_root))
+data_root = project_root / "ml" / "data"
+
 def main():
-    project_root = Path(__file__).resolve().parents[3]
-    sys.path.insert(0, str(project_root))
-    data_root = project_root / "ml" / "data"
 
     split_coco_dataset(
         coco_json_path=data_root / "interim" / "merged" / "train_coco.json",
